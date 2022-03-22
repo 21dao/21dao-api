5e1b2b5b31054923514bdc8470c9ca592692e008
Richard Fisher <richardfsr@gmail.com>
2022-03-22 11:51:39 +0800

    update readme witrh instructions for getting started with development and add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+require 'yaml'
+
+artists = JSON.parse(File.read('./db/artists.json'))
+artists.each do |row|
+  row['tags'] = YAML.safe_load(row['tags'])
+  row['images'] = YAML.safe_load(row['images'])
+end
+Artist.create(artists)
+
+listings = JSON.parse(File.read('./db/listings.json'))
+Listing.create(listings)
+
+auctions = JSON.parse(File.read('./db/auctions.json'))
+Auction.create(auctions)
